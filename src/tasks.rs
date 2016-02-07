3593c7cfdc05ffb703db64de478b6e68af67e4ec
Jaden Carver <jaden.carver@gmail.com>
2016-02-06 22:42:26 -0500

    commonjs javascript compiling

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,10 +1,13 @@
 extern crate argparse;
 extern crate glob;
+extern crate serde_json;
 
+use std::io::prelude::*;
 use std::fs;
 use glob::glob;
 use argparse::{ArgumentParser, StoreTrue, Store};
 use std::process::{Command};
+use std::collections::BTreeMap;
 
 fn main() {
     let mut verbose = false;
@@ -19,37 +22,33 @@ fn main() {
 
     if verbose { println!("Running command {}", command); }
     match command.as_ref() {
-    	"assets" => {
+        "assets" => {
 
             println!("Compiling assets...");
             std::fs::create_dir_all("public/assets").ok();
 
-            for file in glob("app/assets/images/*").unwrap() {
-                match file {
-                    Ok(path) => {
-                        let dest = format!("public/assets/{}", path.file_name().unwrap().to_str().unwrap());
-                        match fs::copy(path, dest.clone()) {
-                            Ok(bytes) => println!("- copied {}", dest),
-                            Err(msg) => println!("- error: {}", msg)
-                        }
-                    },
-                    Err(e) => println!("{:?}", e),
-                }
-            }
-
             Command::new("bower").arg("install").status().unwrap();
 
+            let mut bower_sources: BTreeMap<String, BTreeMap<&str, String>> = BTreeMap::new();
             for file in glob("vendor/assets/*/dist/*.js").unwrap() {
-                match file {
-                    Ok(path) => {
-                        let dest = format!("public/assets/{}", path.file_name().unwrap().to_str().unwrap());
-                        match fs::copy(path, dest.clone()) {
-                            Ok(bytes) => println!("- copied {}", dest),
-                            Err(msg) => println!("- error: {}", msg)
-                        }
-                    },
-                    Err(e) => println!("{:?}", e),
-                }
+                let path = file.unwrap();
+                let name = path.file_stem().unwrap().to_str().unwrap().to_string();
+                let mut config = BTreeMap::new();
+                config.insert("path", path.to_str().unwrap().to_string());
+                bower_sources.insert(name, config);
+            }
+            let mut cjsc_config = fs::File::create("vendor/assets/cjsc.config").unwrap();
+            cjsc_config.write_all(serde_json::to_string(&bower_sources).unwrap().as_ref());
+
+            for file in glob("app/assets/*.js").unwrap() {
+                let path = file.unwrap();
+                let dest_name = path.file_name().unwrap().to_str().unwrap();
+                let dest = format!("public/assets/{}", dest_name);
+
+                Command::new("cjsc").arg("-C").arg("vendor/assets/cjsc.config")
+                    .arg(path.clone()).arg("-M")
+                    .arg("-o").arg(dest)
+                    .status().unwrap();
             }
 
             for file in glob("app/assets/*.scss").unwrap() {
@@ -70,10 +69,10 @@ fn main() {
                 }
             }
 
-    	},
-    	_ => {
-    		println!("Unknown command: {}", command);
-    		std::process::exit(1);
-    	}
+        },
+        _ => {
+            println!("Unknown command: {}", command);
+            std::process::exit(1);
+        }
     }
 }
