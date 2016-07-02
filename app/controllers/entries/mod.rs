c45da4bd11cd48383daeed0cc24357fe795563af
Jaden Carver <jaden.carver@gmail.com>
2016-07-02 04:04:57 -0400

    updates entries

--- app/controllers/entries/mod.rs
+++ app/controllers/entries/mod.rs
@@ -1,23 +1,10 @@
-use iron::prelude::*;
-use iron::modifiers::*;
-use iron::headers;
-use iron::status;
-
-use controllers::params;
-use layouts;
-use formats;
-mod views;
-use params;
-
-use diesel;
-use diesel::prelude::*;
-use models::entry::Entry;
+use controllers::prelude::*;
 use schema::entries::dsl::entries;
-
-//----
+use models::entry::Entry;
+mod views;
 
 pub fn index(request: &mut Request) -> IronResult<Response> {
-    let (route, params, pool) = params(request);
+    let (route, params, pool) = read_request(request);
 
     let entries_per_page: i64 = 5;
     let page = match params.find(&["p"]).unwrap_or(&params::Value::Null).clone() {
@@ -32,18 +19,18 @@ pub fn index(request: &mut Request) -> IronResult<Response> {
 
     Ok(Response::with((status::Ok,
                        Header(formats::html()),
-                       layouts::application(views::index::index(index, page, num_pages))
+                       layouts::entries(views::index::index(index, page, num_pages))
                       )))
 }
 
 pub fn show(request: &mut Request) -> IronResult<Response> {
-    let (route, params, pool) = params(request);
+    let (route, params, pool) = read_request(request);
     let id = route.find("id").unwrap_or("").parse::<i32>().unwrap();
     let ref connection = *pool.get().unwrap();
     let entry = entries.find(id).first::<Entry>(connection).expect("Error loading entry");
     Ok(Response::with((status::Ok,
                        Header(formats::html()),
-                       layouts::application(views::show::show(entry))
+                       layouts::entries(views::show::show(entry))
                       )))
 }
 
@@ -51,23 +38,23 @@ pub fn new(_: &mut Request) -> IronResult<Response> {
     let entry = Entry::new();
     Ok(Response::with((status::Ok,
                        Header(formats::html()),
-                       layouts::application(views::form::new(entry, None))
+                       layouts::entries(views::form::new(entry, None))
                       )))
 }
 
 pub fn edit(request: &mut Request) -> IronResult<Response> {
-    let (route, params, pool) = params(request);
+    let (route, params, pool) = read_request(request);
     let id = route.find("id").unwrap_or("").parse::<i32>().unwrap();
     let ref connection = *pool.get().unwrap();
     let entry = entries.find(id).first::<Entry>(connection).expect("Error loading entry");
     Ok(Response::with((status::Ok,
                        Header(formats::html()),
-                       layouts::application(views::form::edit(entry, None))
+                       layouts::entries(views::form::edit(entry, None))
                       )))
 }
 
 pub fn create(request: &mut Request) -> IronResult<Response> {
-    let (route, params, pool) = params(request);
+    let (route, params, pool) = read_request(request);
     let ref connection = *pool.get().unwrap();
     let mut entry = Entry::new();
     entry.update(params);
@@ -83,14 +70,14 @@ pub fn create(request: &mut Request) -> IronResult<Response> {
         Err(errors) => {
             Ok(Response::with((status::NotAcceptable,
                                Header(formats::html()),
-                               layouts::application(views::form::new(entry, errors))
+                               layouts::entries(views::form::new(entry, errors))
                               )))
         }
     }
 }
 
 pub fn update(request: &mut Request) -> IronResult<Response> {
-    let (route, params, pool) = params(request);
+    let (route, params, pool) = read_request(request);
     let ref connection = *pool.get().unwrap();
     let id = route.find("id").unwrap().parse::<i32>().unwrap();
     let mut entry = entries.find(id).first::<Entry>(connection).expect("Error loading entry");
@@ -107,14 +94,14 @@ pub fn update(request: &mut Request) -> IronResult<Response> {
         Err(errors)  => {
             Ok(Response::with((status::NotAcceptable,
                                Header(formats::html()),
-                               layouts::application(views::form::edit(entry, errors))
+                               layouts::entries(views::form::edit(entry, errors))
                               )))
         }
     }
 }
 
 pub fn delete(request: &mut Request) -> IronResult<Response> {
-    let (route, params, pool) = params(request);
+    let (route, params, pool) = read_request(request);
     //let mut entry = get_entry(request);
     //entry.delete();
     Ok(Response::with((
