fa8f07eae340950c9c44a7035bc6b1099cc7e880
Jaden Carver <jaden.carver@gmail.com>
2016-08-02 20:31:22 -0400

    new link in scaffold

--- src/generators/scaffold/views-index.tmpl.rs
+++ src/generators/scaffold/views-index.tmpl.rs
@@ -11,6 +11,7 @@ pub fn index({resources}: Vec<{Resource}>) -> PreEscaped<String> {{
                 li a href=^(format!("/{resources}/{{}}", {resource}.id())) ^({resource})
             }}
         }}
+        a href="/{resources}/new" {{ "New {Resource}" }}
     }}).unwrap();
 
     PreEscaped(body)
