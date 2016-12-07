7dedfb43f50b71c8cf68a71eded3dafbfe607c4b
Jaden Carver <jaden.carver@gmail.com>
2016-12-07 18:29:21 -0500

    updating dependencies

--- src/monitor.rs
+++ src/monitor.rs
@@ -1,14 +1,16 @@
 extern crate termion;
 extern crate notify;
 extern crate glob;
+extern crate ws;
 
 use termion::{color, style, clear};
-use notify::{RecommendedWatcher, Watcher};
-use std::path::Path;
+use notify::{RecommendedWatcher, Watcher, RecursiveMode};
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::sync::mpsc::channel;
 use std::thread;
 use std::time::Duration;
+use ws::{listen};
 mod tasks;
 
 const ESC: char = 27 as char;
@@ -19,6 +21,7 @@ fn main() {
     let (builder_tx, builder_rx) = channel();
     let (js_assets_tx, js_assets_rx) = channel();
     let (css_assets_tx, css_assets_rx) = channel();
+    //let (reload_tx, reload_rx) = channel();
     let server_path = Path::new("./target/debug/server");
 
     // Initial Build
@@ -27,24 +30,27 @@ fn main() {
     css_assets_tx.send(()).unwrap();
 
     let watcher = thread::spawn(move || {
-        let mut watcher_fs: RecommendedWatcher = Watcher::new(watcher_tx).unwrap();
-        watcher_fs.watch("./app").unwrap();
+        let mut watcher_fs: RecommendedWatcher = Watcher::new(watcher_tx, Duration::from_secs(2)).unwrap();
+        watcher_fs.watch("./app", RecursiveMode::Recursive).unwrap();
         loop {
             let event = watcher_rx.recv().unwrap();
-            let path = event.path.unwrap();
-            if let Some(channel) = match path.extension() {
-                Some(ext) if ext == "rs" => Some(&builder_tx),
-                Some(ext) if ext == "scss" => Some(&css_assets_tx),
-                Some(ext) if ext == "js" => Some(&js_assets_tx),
-                _ => None
-            } {
-                let fg = color::Fg(color::Rgb(0xEE,0xEE,0xEE));
-                let bg = color::Bg(color::Rgb(0x33,0x33,0x33));
-                let (width, height) = termion::terminal_size().unwrap_or((80,25));
-                println!("{}{}{}{}", bg, fg, format!("―――{:―<1$}", format!(" {} ", path.to_str().unwrap()), (width - 3) as usize), style::Reset);
-                channel.send(()).unwrap()
+            let path: Option<PathBuf> = match event {
+              _ => None
+            };
+            if let Some(path) = path {
+              if let Some(channel) = match path.extension() {
+                  Some(ext) if ext == "rs" => Some(&builder_tx),
+                  Some(ext) if ext == "scss" => Some(&css_assets_tx),
+                  Some(ext) if ext == "js" => Some(&js_assets_tx),
+                  _ => None
+              } {
+                  let fg = color::Fg(color::Rgb(0xEE,0xEE,0xEE));
+                  let bg = color::Bg(color::Rgb(0x33,0x33,0x33));
+                  let (width, height) = termion::terminal_size().unwrap_or((80,25));
+                  println!("{}{}{}{}", bg, fg, format!("―――{:―<1$}", format!(" {} ", path.to_str().unwrap()), (width - 3) as usize), style::Reset);
+                  channel.send(()).unwrap()
+              }
             }
-
         }
     });
 
@@ -71,7 +77,7 @@ fn main() {
             thread::sleep(Duration::from_millis(100));
             'flush: loop {
                 match js_assets_rx.try_recv() {
-                    Ok(_) => {}
+                    Ok(_) => {},
                     Err(_) => break 'flush
                 }
             }
@@ -83,6 +89,7 @@ fn main() {
         loop {
             css_assets_rx.recv().unwrap();
             tasks::stylesheets::compile();
+            //reload_tx.send(());
         }
     });
 
@@ -103,6 +110,16 @@ fn main() {
             }
         }
     });
+
+    // TODO: Reload Sockets
+    //listen("127.0.0.1:35729", |out| {
+    //    reload_rx.recv().unwrap();
+    //    println!("sending css");
+    //    out.send("css");
+    //    move |msg| {
+    //        out.send(msg)
+    //    }
+    //});
 
     watcher.join().unwrap();
     builder.join().unwrap();
